95268f2a4c9f7ee525536584c13040b68ff09f0a
DezeG <gromovdeze@yandex.ru>
2021-02-05 23:32:01 +1100

    add error response

--- src/movieSelector.go
+++ src/movieSelector.go
@@ -7,7 +7,7 @@ import(
 func SelectDrmMovies(movies structs.StanRequest) structs.StanResponse {
 	var selectedMovies structs.StanResponse
 	for _, movie := range movies.Payload {
-		if movie.Drm == true {//&& movie.EpisodeCount > 0 {
+		if movie.Drm == true && movie.EpisodeCount > 0 {
 			var buf structs.ResponseMovie = structs.ResponseMovie{}
 			buf.Image = movie.Image.ShowImage
 			buf.Slug = movie.Slug
